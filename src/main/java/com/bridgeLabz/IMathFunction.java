195023d5e8ea01a92f7b6770e04cd37b96c78936
Radhika <reddypallyradhika@gmail.com>
2022-10-08 21:22:08 +0530

    Added Lambda Expression UC1.2

--- src/main/java/com/bridgeLabz/IMathFunction.java
+++ src/main/java/com/bridgeLabz/IMathFunction.java
@@ -2,4 +2,9 @@ package com.bridgeLabz;
 
 public interface IMathFunction {
     int calculate(int a,int b);
+
+    static void printResult(int a, int b, String function, IMathFunction fobj) {
+
+        System.out.println("Result of "+function+"is"+fobj.calculate(a,b));
+    }
 }
--- src/main/java/com/bridgeLabz/MathOperationApp.java
+++ src/main/java/com/bridgeLabz/MathOperationApp.java
@@ -1,6 +1,7 @@
 package com.bridgeLabz;
-//Develop Math Operation App to perform Math Functions
-//– Addition,Subtraction and Division - Use Lambda Expression to perform Math Operation
+//UC1.2
+//Ability to show the results of Math Operation of Addition,Subtraction and Division
+// - Pass Lambda Function to show result
 public class MathOperationApp {
     public static void main(String[] args)
     {
@@ -8,11 +9,17 @@ public class MathOperationApp {
         IMathFunction add =Integer::sum;
         //this expression implements MathFunction
         IMathFunction multiply=(x,y)->x*y;
+        IMathFunction subtract=(x,y)->x*y;
         IMathFunction divide=(x,y)->x/y;
         //add & multiply two numbers using lambda expression
         System.out.println("Addition is : "+add.calculate(5,3));
         System.out.println("Multiplication is :"+multiply.calculate(5,3));
+        System.out.println("Subtraction is :"+subtract.calculate(5,3));
         System.out.println("division is :"+divide.calculate(5,3));
+        //passing LambdaExpression as function parameter to print result using static function
+        IMathFunction.printResult(5,3,"Addition",add);
+        IMathFunction.printResult(5,3,"Subtraction",subtract);
+        IMathFunction.printResult(5,3,"Division",divide);
 
     }
 }
